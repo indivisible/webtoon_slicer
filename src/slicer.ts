f1e2d949112a0ff7059da1228e953df163d1ff8a
Zoltán <islandofcalmness@gmail.com>
2022-06-17 23:21:57 +0200

    remove now unused function dataURLToBlob

--- src/slicer.ts
+++ src/slicer.ts
@@ -514,27 +514,6 @@ function renderSlice(y1: number, y2: number) {
   return canvas;
 }
 
-function dataURLtoBlob(dataURI: string) {
-  // convert base64/URLEncoded data component to raw binary data held in a string
-  let byteString;
-  if (dataURI.split(',')[0].indexOf('base64') >= 0) {
-    byteString = atob(dataURI.split(',')[1]);
-  } else {
-    byteString = unescape(dataURI.split(',')[1]);
-  }
-
-  // separate out the mime component
-  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-  // write the bytes of the string to a typed array
-  const ia = new Uint8Array(byteString.length);
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  return new Blob([ia], {type: mimeString});
-}
-
 function downloadBlob(blob: Blob, name: string) {
   const a = document.createElement('a');
   a.download = name;
